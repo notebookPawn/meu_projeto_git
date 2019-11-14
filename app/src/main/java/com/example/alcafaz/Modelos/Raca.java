07bbc97463aefb1f6286beca26b7b992067b4f14
Rodrigo <rodrigosbranco26@gmail.com>
2019-11-14 01:07:54 +0000

    Criação do modelo Raça
    Criação das variáveis da raça, assim como as funções get e set das mesmas

---
+++ app/src/main/java/com/example/alcafaz/Modelos/Raca.java
@@ -0,0 +1,62 @@
+package com.example.alcafaz.Modelos;
+
+public class Raca {
+    private String name;
+    private int maxAge;
+    private double maxSize, speed;
+    private int[] attributes = new int[6];
+    private String[] proficiencies, Languages;
+
+    public Raca(String name) {
+        this.name = name;
+    }
+
+    public int[] getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma ){
+        int[] raceAtributes = {strength, dexterity, constitution, intelligence, wisdom, charisma};
+        attributes = raceAtributes;
+    }
+
+    public String[] getProficiencies() {
+        return proficiencies;
+    }
+
+    public void setProficiencies(String[] proficiencies) {
+        this.proficiencies = proficiencies;
+    }
+
+    public String[] getLanguages() {
+        return Languages;
+    }
+
+    public void setLanguages(String[] languages) {
+        Languages = languages;
+    }
+
+    public int getMaxAge() {
+        return maxAge;
+    }
+
+    public void setMaxAge(int maxAge) {
+        this.maxAge = maxAge;
+    }
+
+    public double getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(double maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+}
